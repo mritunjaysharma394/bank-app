faae7cce34ccaaf4126b07b4137517bcaefb2e40
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2021-01-13 23:59:44 +0530

    add navigation

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ function updateRoute() {
     const path = window.location.pathname;
     const route = routes[path];
 
+    if(!route) {
+        return navigate('/login');
+    }
+
     const template = document.getElementById(route.templateId);
     const view = template.content.cloneNode(true);
     const app = document.getElementById('app');
@@ -14,5 +18,12 @@ function updateRoute() {
     app.appendChild(view);
 }
 
+function navigate(path) {
+    window.history.pushState({}, path, window.location.origin + path);
+    updateRoute();
+}
 
-//updateRoute();
+function onLinkClick(event) {
+    event.preventDefault();
+    navigate(event.target.href);
+}
